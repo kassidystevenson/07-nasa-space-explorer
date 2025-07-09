1f3acc094b8b6de86584f0d6d3de3e3eda41cb93
Kassidy Stevenson <stevensonkn@appstate.edu>
2025-07-09 00:07:12 +0000

    Fix formatting in script.js for consistency

--- js/script.js
+++ js/script.js
@@ -6,4 +6,4 @@ const endInput = document.getElementById('endDate');
 // This sets up the date pickers to:
 // - Default to a range of 9 days (from 9 days ago to today)
 // - Restrict dates to NASA's image archive (starting from 1995)
-setupDateInputs(startInput, endInput);
+setupDateInputs(startInput, endInput); 
